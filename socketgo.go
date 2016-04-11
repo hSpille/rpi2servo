469fd7b766315e8471a6cab0c2377edf0a14e29f
henning <h.spille@gmail.com>
2016-04-11 12:27:27 +0200

    Initial

---
+++ socketgo.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net"
+	"time"
+)
+
+func reader(r io.Reader) {
+	buf := make([]byte, 1024)
+	for {
+		n, err := r.Read(buf[:])
+		if err != nil {
+			return
+		}
+		println("Client got:", string(buf[0:n]))
+	}
+}
+
+func main() {
+	socketLocation := "/tmp/python_socket.sock"
+
+	//https://golang.org/pkg/net/#Dial
+	c, err := net.Dial("unixgram", socketLocation)
+	if err != nil {
+		panic(err)
+	}
+	defer c.Close()
+
+	go reader(c)
+	for {
+		_, err := c.Write([]byte("hi"))
+		if err != nil {
+			log.Fatal("write error:", err)
+			break
+		}
+		time.Sleep(1e9)
+	}
+}
---
+++ sockettest.py
@@ -0,0 +1,64 @@
+from __future__ import with_statement # Required in 2.5
+import socket
+import os, os.path
+import time
+import signal
+import sys
+from contextlib import contextmanager
+
+SocketAdr = "/tmp/python_socket.sock"
+StopString = "HALT"
+TimeOutSeconds = 4
+
+class TimeoutException(Exception): pass
+
+@contextmanager
+def time_limit(seconds):
+    def signal_handler(signum, frame):
+        raise TimeoutException()
+    signal.signal(signal.SIGALRM, signal_handler)
+    signal.alarm(seconds)
+    try:
+        yield
+    finally:
+        signal.alarm(0)
+
+
+def listen():
+    datagram = server.recv( 1024 )
+    if not datagram:
+        print("strange things happen... better stop")
+        return StopString
+    return datagram.decode("utf-8")
+
+def actuate(instruction):
+    sys.stdout.write("\rInstruction: " + instruction )
+    sys.stdout.flush()
+
+
+
+#Begin main
+#Stopping first
+actuate(StopString)
+if os.path.exists( SocketAdr):
+	os.remove( SocketAdr )
+server = socket.socket( socket.AF_UNIX, socket.SOCK_DGRAM )
+server.bind(SocketAdr)
+ 
+print("\nListening for first connection")
+datagram = server.recv( 1024 )
+print("Here we go")
+doListen = True;
+while doListen:
+    instruction = StopString
+    try:
+        with time_limit(TimeOutSeconds):
+            instruction = listen()
+    except TimeoutException:
+        print ("Close after TimeoutException")
+        doListen = False
+    actuate(instruction)
+#Ending
+server.close()
+os.remove(SocketAdr)
+print ("bye")
