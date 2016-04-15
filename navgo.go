ef331703677503595a4abeb5fae178cec66a094c
henning <h.spille@gmail.com>
2016-04-15 22:34:37 +0200

    Channel angepasst

--- navgo.go
+++ navgo.go
@@ -19,40 +19,52 @@ func reader(r io.Reader) {
 	}
 }
 
-func main() {
+func readFromGui(guiChan chan<- string) {
 	ServerAddr, err := net.ResolveUDPAddr("udp", ":10001")
-
+	if err != nil {
+		panic(err)
+	}
 	/* Now listen at selected port */
 	ServerConn, err := net.ListenUDP("udp", ServerAddr)
 	defer ServerConn.Close()
-
 	buf := make([]byte, 1024)
-
 	for {
 		n, addr, err := ServerConn.ReadFromUDP(buf)
-		fmt.Println("Received ", string(buf[0:n]), " from ", addr)
-
+		fmt.Println("FromGui ", string(buf[0:n]), " from ", addr)
+		guiChan <- string(buf[0:n])
 		if err != nil {
 			fmt.Println("Error: ", err)
 		}
 	}
+}
 
+func main() {
+	guiChannel := make(chan string, 1)
+	go readFromGui(guiChannel)
 	socketLocation := "/tmp/python_socket.sock"
-
 	//https://golang.org/pkg/net/#Dial
 	c, err := net.Dial("unixgram", socketLocation)
 	if err != nil {
 		panic(err)
 	}
 	defer c.Close()
-
 	go reader(c)
 	for {
-		_, err := c.Write([]byte("hi"))
-		if err != nil {
-			log.Fatal("write error:", err)
+		select {
+		case msg := <-guiChannel:
+			fmt.Println("received message", msg)
+			_, err := c.Write([]byte(msg))
+			if err != nil {
+				panic(err)
+			}
 			break
+		case _ = <-time.NewTicker(1000 * time.Millisecond).C:
+			fmt.Println("no message from Gui")
+			_, err := c.Write([]byte("nop"))
+			if err != nil {
+				log.Fatal("write error:", err)
+				break
+			}
 		}
-		time.Sleep(1e9)
 	}
 }
