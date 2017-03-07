c41e01796b23887c1cbb8c4c02e0ceb40c768720
hspille <h.spille@gmail.com>
2017-03-07 13:15:01 +0100

    Loeschen von Verbindungen wenn diese nicht mehr exisiteren

--- gps/gps.go
+++ gps/gps.go
@@ -7,7 +7,7 @@ package main
 import "C"
 import "fmt"
 import "net"
-import 	"encoding/json"
+import "encoding/json"
 import "os"
 import "sync"
 
@@ -16,7 +16,6 @@ type GpsMessage struct {
 	Long  float64
 	Speed float64
 }
-
 
 type GpsLocationCallback func(float64, float64, float64) bool
 
@@ -32,14 +31,13 @@ func GpsLocation(cb GpsLocationCallback) {
 			lon := float64(data.fix.longitude)
 			speed := float64(data.fix.speed)
 			if cb(lat, lon, speed) {
-				break;
+				break
 			}
 		} else {
 			fmt.Println("No GPS fix:", data.fix.mode)
 		}
 	}
 }
-
 
 func readGps(conn *net.UnixConn) {
 	defer conn.Close()
@@ -51,16 +49,15 @@ func readGps(conn *net.UnixConn) {
 		}
 		_, err = conn.Write([]byte(jsonGps))
 		if err != nil {
-			fmt.Println("Client disconnect:" , err)
+			fmt.Println("Client disconnect:", err)
 			return true
 		}
 		return false
 	})
 }
 
-
 func main() {
-	l, err := net.ListenUnix("unix",&net.UnixAddr{"/tmp/gps_socket", "unix"})
+	l, err := net.ListenUnix("unix", &net.UnixAddr{"/tmp/gps_socket", "unix"})
 	if err != nil {
 		panic(err)
 	}
@@ -70,9 +67,9 @@ func main() {
 
 	go func() {
 		defer os.Remove("/tmp/gps_socket")
-		for{
+		for {
 			conn, err := l.AcceptUnix()
-			if err != nil{
+			if err != nil {
 				panic(err)
 			}
 
@@ -83,9 +80,9 @@ func main() {
 			// b := []byte("hi")
 			// _, err = conn.Write(b)
 			// readGps(conn)
-	  //       if err != nil {
-	  //           panic(err)
-	  //       }
+			//       if err != nil {
+			//           panic(err)
+			//       }
 		}
 	}()
 
@@ -95,13 +92,20 @@ func main() {
 		if err != nil {
 			fmt.Println("Error", err)
 		}
-		var toRemove []*net.UnixConn
+		var toRemove []int
 		connectionsMu.Lock()
+		var index int = 0
 		for _, conn := range connections {
+			index++
 			_, err = conn.Write([]byte(jsonGps))
 			if err != nil {
-				fmt.Println("Client disconnect:" , err)
-				toRemove =append(conn)
+				fmt.Println("Client disconnect:", err)
+				toRemove = append(toRemove, index)
+			}
+		}
+		if len(toRemove) > 0 {
+			for i := 0; i < index; i++ {
+				connections = append(connections[:i], connections[i+1:]...)
 			}
 		}
 		connectionsMu.Unlock()
